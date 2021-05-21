ce89a1f31afa8bb0045c641ae5b8598c66103787
alexander.siguenza <alexander.siguenza@gmail.com>
2021-05-20 22:35:51 -0600

    Se agrego mostrar index

--- app/Http/Controllers/EmpleadoController.php
+++ app/Http/Controllers/EmpleadoController.php
@@ -14,7 +14,8 @@ class EmpleadoController extends Controller
      */
     public function index()
     {
-        return view ('empleado.index');
+        $datos['empleado'] =Empleado::paginate(5);
+        return view ('empleado.index',$datos);
     }
 
     /**
--- resources/views/empleado/index.blade.php
+++ resources/views/empleado/index.blade.php
@@ -1,1 +1,29 @@
 // Mostrar la lista de empleados :)
+<table class="table table-light">
+
+    <thead class="thead-light">
+        <tr>
+            <th>Foto</th>
+            <th>Nombre</th>
+            <th>Apellido Paterno</th>
+            <th>Apellido materno</th>
+            <th>Correo</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+
+    <tbody>
+    @foreach($empleado as $empleado)
+        <tr>      
+            <td>{{ $empleado->id }}</td>
+            <td>{{ $empleado->Foto }}</td>
+            <td>{{ $empleado->Nombre }}</td>
+            <td>{{ $empleado->ApellidoPaterno }}</td>
+            <td>{{ $empleado->ApellidoMaterno }}</td>
+            <td>{{ $empleado->Correo }}</td>
+            <td>Editar | Borrar</td>
+        </tr>
+        @endforeach
+    </tbody>
+
+</table>
